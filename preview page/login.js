55897c97f2ef2fc16d4082adfb97b4386a6b81d0
vrnsphinxx <vernafed@gmail.com>
2024-03-26 20:54:34 +0800

    login_auth

---
+++ preview page/login.js
@@ -0,0 +1,46 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDTeKSFZF9qGWCJqHXev9Yj2Man36IDgx4",
+  authDomain: "a-do-ff29e.firebaseapp.com",
+  databaseURL: "https://a-do-ff29e-default-rtdb.firebaseio.com",
+  projectId: "a-do-ff29e",
+  storageBucket: "a-do-ff29e.appspot.com",
+  messagingSenderId: "488739423620",
+  appId: "1:488739423620:web:9bdc3605a45a3714b249d1",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
+//google sign in
+const googleSignInBtn = document.getElementById("googleSign");
+
+googleSignInBtn.addEventListener("click", () => {
+  const provider = new GoogleAuthProvider();
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // User successfully signed in.
+      const user = result.user;
+      window.location.href = "../home page/homepage.html";
+    })
+    .catch((error) => {
+      // Error occurred during sign-in.
+      console.error("Error");
+    });
+  signOut(auth)
+    .then(() => {
+      // Sign-out successful.
+    })
+    .catch((error) => {
+      // An error happened.
+    });
+});
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  devtool: "eval-source-map",
+  entry: "../preview page/login.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
