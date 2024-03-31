3c25a1a93f1dc57f35619c0eb7634c55af04c476
aicla <airajpn06@gmail.com>
2024-04-01 07:00:54 +0800

    making task

--- tasks page/task.js
+++ tasks page/task.js
@@ -21,3 +21,40 @@ function toggleOptions(optionClass) {
         options.classList.remove('active');
     }
 }
+
+document.getElementById("kid_star_button").addEventListener("click", function() {
+    var icon = document.getElementById("kid_star_icon");
+    if (icon.classList.contains("filled")) {
+        icon.classList.remove("filled");
+    } else {
+        icon.classList.add("filled");
+    }
+    });
+
+
+const dropdowns = document.querySelectorAll('.teams');
+
+dropdowns.forEach(teams => {
+    const select = teams.querySelector('.select');
+    const caret = teams.querySelector('.caret');
+    const menu = teams.querySelector('.dropdown');
+    const options = teams.querySelectorAll('.dropdown li');
+    const selected = teams.querySelector('.selected');
+    
+    select.addEventListener('click', () => {
+        caret.classList.toggle('caret-rotate');
+        menu.classList.toggle('menu-open');
+    });
+
+    options.forEach(option => {
+        option.addEventListener('click', () => {
+            selected.innerText = option.innerText;
+            caret.classList.remove('caret-rotate');
+            menu.classList.remove('menu-open');
+            options.forEach(option => {
+                option.classList.remove('chosen');
+            });
+            option.classList.add('chosen');
+        });
+    });
+});
