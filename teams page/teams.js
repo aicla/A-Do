6067855eca07aef55ad06c2d36622010bff4c813
Shineee11 <105972720+Shineee11@users.noreply.github.com>
2024-04-08 07:37:10 +0800

    teams update

--- teams page/teams.js
+++ teams page/teams.js
@@ -1,3 +1,56 @@
+// Get the modals
+var modal = document.getElementById("modal");
+var membersModal = document.getElementById("members-modal");
+var addTeamModal = document.getElementById("add-team-modal");
+
+// Get the back buttons
+var backButton = modal.querySelector(".back-button");
+var membersBackButton = membersModal.querySelector(".back-button");
+var addTeamBackButton = addTeamModal.querySelector(".cancel-btn");
+
+// Get the button that opens the modal
+var boxes = document.querySelectorAll(".box .team");
+
+// Loop through each team box and add click event listener
+boxes.forEach(function(box) {
+    box.addEventListener("click", function() {
+        modal.style.display = "block";
+        hideScrollbar(); 
+    });
+});
+
+// When the user clicks on the back button in the team content modal, close the modal
+backButton.onclick = function() {
+    modal.style.display = "none";
+    showScrollbar();
+};
+
+// When the user clicks on the "Members" button, open the members modal
+var membersButton = document.querySelector(".members-btn");
+membersButton.onclick = function() {
+    membersModal.style.display = "block";
+    hideScrollbar(); 
+};
+
+// When the user clicks on the back button in the members modal, close the modal
+membersBackButton.onclick = function() {
+    membersModal.style.display = "none";
+    hideScrollbar();
+};
+
+// When the user clicks on the add icon, open the add team modal
+var addIcon = document.querySelector(".add");
+addIcon.onclick = function() {
+    addTeamModal.style.display = "block";
+    hideScrollbar(); 
+};
+
+// When the user clicks on the CANCEL button in the add team modal, close the modal
+addTeamBackButton.onclick = function() {
+    addTeamModal.style.display = "none";
+    showScrollbar();
+};
+
 // Function to hide scrollbar
 function hideScrollbar() {
     document.body.style.overflow = 'hidden';
@@ -7,26 +60,3 @@ function hideScrollbar() {
 function showScrollbar() {
     document.body.style.overflow = '';
 }
-
-// Get the modal
-var modal = document.getElementById("modal");
-
-// Get the back button
-var backButton = document.querySelector(".back-button");
-
-// Get the button that opens the modal
-var boxes = document.querySelectorAll(".box .team");
-
-// Loop through each team box and add click event listener
-boxes.forEach(function(box) {
-    box.addEventListener("click", function() {
-        modal.style.display = "block";
-        hideScrollbar(); // Hide the scrollbar when modal appears
-    });
-});
-
-// When the user clicks on the back button, close the modal
-backButton.onclick = function() {
-    modal.style.display = "none";
-    showScrollbar(); // Show the scrollbar when modal is closed
-};
