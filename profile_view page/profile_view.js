5d47d7f454a5d99de9b9fcf37868cb8de2b04790
vrnsphinxx <vernafed@gmail.com>
2024-04-08 16:46:34 +0800

    modified:   profile_view page/profile_view.css 	modified:   profile_view page/profile_view.html 	modified:   profile_view page/profile_view.js

--- profile_view page/profile_view.js
+++ profile_view page/profile_view.js
@@ -1,25 +1,3 @@
-import {
-  getAuth,
-  signOut,
-} from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
-import firebaseConfig from "../home page/homepage.js";
-import logoutButton from "../home page/homepage.js";
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-logoutButton.addEventListener("click", () => {
-  signOut(auth)
-    .then(() => {
-      console.log("Sign out");
-      window.location.href = "../preview page/login.html";
-    })
-    .catch((error) => {
-      console.error("Sign-out error:", error);
-    });
-});
-
 document.addEventListener("DOMContentLoaded", function () {
   // Fetch uid, photoURL, and displayName from localStorage
   const uid = localStorage.getItem("uid");
