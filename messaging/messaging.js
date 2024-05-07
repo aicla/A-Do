45d18a39b2044c15639feadd101e62d99624a8a1
aicla <airajpn06@gmail.com>
2024-05-08 04:43:54 +0800

    ui

--- messaging/messaging.js
+++ messaging/messaging.js
@@ -1,31 +1,29 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
-import {
-  getDatabase,
-  ref,
-  push,
-  onChildAdded,
-} from "https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-storage.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDTeKSFZF9qGWCJqHXev9Yj2Man36IDgx4",
   authDomain: "a-do-ff29e.firebaseapp.com",
-  databaseURL: "https://a-do-ff29e-default-rtdb.firebaseio.com",
   projectId: "a-do-ff29e",
   storageBucket: "a-do-ff29e.appspot.com",
   messagingSenderId: "488739423620",
-  appId: "1:488739423620:web:9bdc3605a45a3714b249d1",
+  appId: "1:488739423620:web:9bdc3605a45a3714b249d1"
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-const storage = getStorage(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const colRef = collection(db, 'messages')
 
-const displayName = localStorage.getItem("displayName");
-
-const username = document.getElementById("username");
-  if (username) {
-    username.textContent = displayName;
-  } else {
-    console.error("Username element not found");
-  }
+getDocs(colRef)
+  .then((snapshot) => {
+    let messages = []
+    snapshot.docs.forEach((doc) => {
+      messages.push({ ...doc.data(), id: doc.id })
+    })
+    console.log(messages)
+    })
+    .catch(err => {
+      console.log(err.message)
+  })
